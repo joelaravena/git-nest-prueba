8d426ae38e3d879fe95eb48fb1a7a07e6cc81e05
Joel <skaup.joel@gmail.com>
2020-11-02 10:29:08 -0300

    ci update v8

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Joel ya esta listo el CI/CD v1';
+    return 'Hello World! Joel ya esta listo el CI/CD v1 buena :D';
   }
 }
